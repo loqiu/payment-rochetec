77bff5daac4c7849b2ee57f16ce81661313db86e
Chongjin_Wang <loqiu_software@163.com>
2024-12-15 00:17:45 +0000

    payment-project init

---
+++ src/main/java/com/rochetec/paymentrochetec/PaymentRochetecApplication.java
@@ -0,0 +1,13 @@
+package com.rochetec.paymentrochetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRochetecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentRochetecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rochetec/paymentrochetec/PaymentRochetecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rochetec.paymentrochetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRochetecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
